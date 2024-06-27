a49b971a18508d1b0aecd3b0274de30102ed1faf
Stilllee <orodae@gmail.com>
2024-06-28 00:09:07 +0900

    입력폼 만들기
    - `form`태그와 `input`태그를 이용하여 `이름 / 희망연봉` 입력폼 만들기
    - 마지막에 `submit`”전송” 버튼 추가

--- src/app/wanted/post/page.tsx
+++ src/app/wanted/post/page.tsx
@@ -1,3 +1,19 @@
 export default function Post() {
-  return <div>Wanted Post</div>;
+  return (
+    <div>
+      <form action="">
+        <ul>
+          <li>
+            이름 <input type="text" />
+          </li>
+          <li>
+            희망연봉 <input type="number" />
+          </li>
+        </ul>
+        <button className="bg-blue-300 text-white font-semibold p-3 rounded-md">
+          전송
+        </button>
+      </form>
+    </div>
+  );
 }
