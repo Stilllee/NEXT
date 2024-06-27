9b6710ab9b1de5eaf028136183bf476d0cb0b73c
Stilllee <orodae@gmail.com>
2024-06-28 00:18:04 +0900

    handleForm 추가 후 form의 action 태그에 연결

--- src/app/wanted/post/page.tsx
+++ src/app/wanted/post/page.tsx
@@ -1,13 +1,20 @@
+async function handleForm(formData: FormData) {
+  "use server";
+
+  console.log(formData.get("name"), formData.get("salary"));
+  console.log("I run the server");
+}
+
 export default function Post() {
   return (
     <div>
-      <form action="">
+      <form action={handleForm}>
         <ul>
           <li>
-            이름 <input type="text" />
+            이름 <input type="text" name="name" />
           </li>
           <li>
-            희망연봉 <input type="number" />
+            희망연봉 <input type="number" name="salary" />
           </li>
         </ul>
         <button className="bg-blue-300 text-white font-semibold p-3 rounded-md">
