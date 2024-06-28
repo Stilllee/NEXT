2057756500141325d6e1517d071f7e43170213fc
Stilllee <orodae@gmail.com>
2024-06-29 00:44:18 +0900

    [Dynamic Rendering] next/cache의 noStore()
    - 항상 최신 데이터를 가져오는 방법

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,8 +8,10 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
